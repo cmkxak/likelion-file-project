3d8d8cacc9aca001211414f29bfc81968da31cf5
cmkxak <gcm466@naver.com>
2022-10-13 17:50:35 +0900

    리팩토링 : 불필요한 코드 제거 및 테스트 메소드명 변경

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,27 +8,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileManager<Hospital> fileManager = new FileManager<Hospital>(new SqlParser()); //구현체 주입 -> 다형성 적용
-//      String fileName = "C:\\users\\cmkxak\\Downloads\\서울시 병의원 위치 정보.csv"; //서울시 병의원 위치 정보
+        //구현체 주입 -> 다형성 적용
+        FileManager<Hospital> fileManager = new FileManager<Hospital>(new SqlParser());
 
         String dataFileName = "seoul_data.txt";
-
         String sqlFileName = "seoul_hospital.sql";
-        List<Hospital> sqlFile = fileManager.readFiles(dataFileName); //sql 파일 생성 관련
-        fileManager.write(sqlFile, sqlFileName); //sql 파일 내용 작성
 
-//      List<Hospital> hospitalFile = fileManager.readFiles(fileName); //데이터 생성 파일 관련
-
-//        System.out.println(hospitalFile.size());
-
-        //hospital id
-//        for (Hospital hospital : hospitalFile) {
-//            System.out.println(hospital);
-//        }
-
-//        fileManager.createAFile(dataFileName); //파일 생성
-//        fileManager.createAFile(sqlFileName); //sql 파일 생성
-
-//        fileManager.write(hospitalFile, dataFileName); //파일 내용 작성
+        //sql 파일 생성 관련
+        List<Hospital> sqlFile = fileManager.readFiles(dataFileName);
+        fileManager.write(sqlFile, sqlFileName);
     }
 }
--- src/test/java/org/example/parser/HospitalParserTest.java
+++ src/test/java/org/example/parser/HospitalParserTest.java
@@ -10,7 +10,7 @@ class HospitalParserTest {
     String str2 = String.format("\"A1120837\",\"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\",\"C\",\"의원\",\"G099\",\"응급의료기관 이외\",\"2\",\"외과: 상시진료 내과는 당분간 휴진\",\"서울시 송파구 문정동 장지동 법조단지 위례 가락동 가락시장역 위치 삼성서울병원 외래교수 출신 구강외과 전문의 진료 진료과목 - 임플란트 치조골 뼈이식 수술 매복 사랑니 발치 턱관절 악관절 질환의 치료 교정 치료 및 기타 보존 보철(크라운 브릿지 인레이) 신경치료\",\"방이역 1번출구 바로옆 굿모닝 신한증권 뒷건물\",\"가산기대찬의원\",\"02-6267-2580\",\"02-920-5374\",\"1930\",\"1930\",\"1930\",\"1930\",\"1930\",\"1500\",\"1500\",\"1500\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"0900\",\"1000\",\"1000\",\"085\",null");
 
     @Test
-    void id(){
+    void id_test(){
         HospitalParser hospitalParser = new HospitalParser();
 
         Hospital hospital = hospitalParser.parse(str2);
@@ -18,15 +18,16 @@ class HospitalParserTest {
         assertEquals("A1120837", hospital.getId());
     }
     @Test
-    void address(){
+    void address_test(){
         String address = "서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)";
         HospitalParser hospitalParser = new HospitalParser();
 
         Hospital parse = hospitalParser.parse(str2);
         assertEquals(address, parse.getAddress());
     }
+
     @Test
-    void district(){
+    void district_test(){
         String district = "서울특별시 금천구";
         HospitalParser hospitalParser = new HospitalParser();
 
@@ -35,7 +36,7 @@ class HospitalParserTest {
     }
 
     @Test
-    void category() {
+    void category_test() {
         HospitalParser hospitalParser = new HospitalParser();
         String category = "C";
         Hospital hospitalParse = hospitalParser.parse(str2);
@@ -43,7 +44,7 @@ class HospitalParserTest {
     }
 
     @Test
-    void emergencyRoom(){
+    void emergencyRoom_test(){
         Integer emergencyRoomStatus = 2;
         HospitalParser hospitalParser = new HospitalParser();
 
@@ -52,11 +53,10 @@ class HospitalParserTest {
     }
 
     @Test
-    void name() {
+    void name_test() {
         HospitalParser hospitalParser = new HospitalParser();
         String name = "가산기대찬의원";
         Hospital hospitalParse = hospitalParser.parse(str2);
         assertEquals(name, hospitalParse.getName());
     }
-
 }
