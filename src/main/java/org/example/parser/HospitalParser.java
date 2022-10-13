d85bb19f285fdd656401ac2cbc0e95f446ac08d9
cmkxak <gcm466@naver.com>
2022-10-13 13:20:07 +0900

    address test code 추가 및 큰따옴표 제거

--- src/main/java/org/example/parser/HospitalParser.java
+++ src/main/java/org/example/parser/HospitalParser.java
@@ -3,13 +3,17 @@ package org.example.parser;
 import org.example.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital>{
+    private String replaceAllQuot(String str){
+        return str.replaceAll("\"" , "");
+    }
+
     @Override
     public Hospital parse(String fileContents) {
         String[] split = fileContents.split(",");
 
         int emergency = Integer.parseInt(split[6].replaceAll("\"", ""));
 
-        return new Hospital(split[0], split[1], split[2],
-                emergency, split[10], null);
+        return new Hospital(replaceAllQuot(split[0]), replaceAllQuot(split[1]), replaceAllQuot(split[2]),
+                emergency, replaceAllQuot(split[10]), null);
     }
 }
