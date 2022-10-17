b8cbc4c6b91f019fe23903db245b977cf5e66222
cmkxak <gcm466@naver.com>
2022-10-17 13:02:07 +0900

    select 구문 추가 및 jdbc 필요 변수 설정

--- src/main/java/org/example/dao/UserDao.java
+++ src/main/java/org/example/dao/UserDao.java
@@ -3,18 +3,20 @@ package org.example.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Map;
 
 public class UserDao {
+
+    private final String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+
+    //보안을 위해 설정한 환경변수 값들을 map으로 가져오기
+    private Map<String, String> env = System.getenv();
+    private String dbHost = env.get("DB_HOST");
+    private String dbUser = env.get("DB_USER");
+    private String dbPassword = env.get("DB_PASSWORD");
+
     public void add(){
-        //보안을 위한 설정한 환경변수 값들을 map으로 가져오기
-        Map<String, String> env = System.getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
-
-        String jdbcDriver = "com.mysql.cj.jdbc.Driver";
-
         try{
             //jdbc 로드
             Class.forName(jdbcDriver);
@@ -25,8 +27,8 @@ public class UserDao {
 
             //2. 쿼리 작성
             PreparedStatement ps = c.prepareStatement("INSERT INTO users values(?,?,?)");
-            ps.setString(1, "1");
-            ps.setString(2, "홍길동");
+            ps.setString(1, "2");
+            ps.setString(2, "김길동");
             ps.setString(3, "password");
 
             //3. 쿼리 실행
@@ -41,8 +43,35 @@ public class UserDao {
         }
     }
 
+    public void select(){
+        try{
+            Class.forName(jdbcDriver);
+
+            Connection c = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+
+            PreparedStatement ps = c.prepareStatement("select * from users where id = \"1\"");
+
+            //결과 가져오기
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                System.out.printf("%s\t%s\t%s\t\n"
+                        ,rs.getString("id")
+                        ,rs.getString("name")
+                        ,rs.getString("password"));
+            }
+
+            ps.close();
+            c.close();
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
         UserDao userDao = new UserDao();
-        userDao.add();
+//        userDao.add();
+        userDao.select();
     }
 }
