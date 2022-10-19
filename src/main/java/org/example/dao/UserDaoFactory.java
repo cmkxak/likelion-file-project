6ccfd67a9cd994752c355d83f314aa3732a7d34a
cmkxak <gcm466@naver.com>
2022-10-19 13:41:35 +0900

    구현체 설정 클래스 @Configuration으로 등록 및 빈 등록

--- src/main/java/org/example/dao/UserDaoFactory.java
+++ src/main/java/org/example/dao/UserDaoFactory.java
@@ -2,13 +2,17 @@ package org.example.dao;
 
 import org.example.connection.AwsConnectionMaker;
 import org.example.connection.LocalDbConnectionMaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class UserDaoFactory {
+    @Bean
     public UserDao awsUserDao(){
         UserDao userDao = new UserDao(new AwsConnectionMaker());
         return userDao;
     }
-
+    @Bean
     public UserDao localUserDao(){
         UserDao userDao = new UserDao(new LocalDbConnectionMaker());
         return userDao;
